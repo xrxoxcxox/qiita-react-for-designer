2b1d90c12bfafa70f1d2a7598826c47a9657aa9c
Keisuke Watanuki <watanuki.keisuke@a-tm.co.jp>
2021-04-09 01:32:35 +0900

    Add Routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import SomeComponent from './SomeComponent'
 
 const firstName = "John"
 const lastName = "Doe"
@@ -7,22 +14,60 @@ const fullName = `${firstName} ${lastName}`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello, {fullName}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hello, {fullName}
+          </p>
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/users">
+              <Users />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+          <ul className="linkList">
+            <li className="linkItem">
+              <Link to="/" className="link">Home</Link>
+            </li>
+            <li className="linkItem">
+              <Link to="/about" className="link">About</Link>
+            </li>
+            <li className="linkItem">
+              <Link to="/users" className="link">Users</Link>
+            </li>
+          </ul>
+        </header>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return (
+    <>
+      <h2>About</h2>
+      <SomeComponent />
+    </>
+  );
+}
+
+function Users() {
+  return (
+    <>
+      <h2>Users</h2>
+      <SomeComponent />
+    </>
   );
 }
 
---
+++ src/SomeComponent.js
@@ -0,0 +1,7 @@
+function SomeComponent() {
+  return (
+    <p style={{ fontSize: 20, color: '#61dafb' }}>This is example component</p>
+  )
+}
+
+export default SomeComponent
